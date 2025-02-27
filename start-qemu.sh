b483320da32625b9b6547254eba833570ea2c5d5
MocusEZ <mocusz@163.com>
2025-02-28 10:40:50 +1100

    refactor: replace startup script in Dockerfile and adjust QEMU memory allocation

--- start-qemu.sh
+++ start-qemu.sh
@@ -1,10 +1,10 @@
-qemu-system-riscv64 \
+screen -dmS qemu qemu-system-riscv64 \
     -machine virt \
     -cpu rv64,zba=true,zbb=true,v=true,vlen=256,vext_spec=v1.0,rvv_ta_all_1s=true,rvv_ma_all_1s=true \
     -smp 4 \
-    -m 16G \
+    -m 4G \
     -device virtio-blk-device,drive=hd \
-    -drive file=image.qcow2,if=none,id=hd \
+    -drive file=dqib_riscv64-virt/image.qcow2,if=none,id=hd \
     -device virtio-net-device,netdev=net \
     -netdev user,id=net,hostfwd=tcp::2222-:22 \
     -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf \
